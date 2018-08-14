b9bca01664b41fc1d8cd9e22c040e241078c2ec2
Brandon Phillips <brandon.phillips@autoipacket.com>
2018-08-13 20:58:33 -0400

    add more test tasks

--- app_celery/tasks.py
+++ app_celery/tasks.py
@@ -1,4 +1,6 @@
 from celery import Celery
+from time import sleep
+
 
 app = Celery('tasks', broker='pyamqp://guest@localhost')
 
@@ -6,3 +8,9 @@ app = Celery('tasks', broker='pyamqp://guest@localhost')
 @app.task
 def add(x, y):
     return x + y
+
+@app.task
+def count(range):
+    for i in range(range):
+        print(i)
+        sleep(1)
